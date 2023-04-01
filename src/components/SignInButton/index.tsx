4714207043919eacced584f4168367f0a730bbdd
Marlon Andrei <marlon-master@hotmail.com>
2023-03-31 23:49:23 -0300

    useSession Fixed

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,23 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-import { signIn, signOut, useSession } from 'next-auth/client';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
     // autentication
-    const [session] = useSession();
+    const { data } = useSession();
 
     // o return já é algo condicional
-    return session ? (
+    return data ? (
         <button
             className={styles.signInButton}
             type="button"
             onClick={() => signOut()}
         >
             <FaGithub color="#04d361" />
-            {session.user.name}
-            <FiX color="#737380" className={styles.closeIcon} />
+            {data.user.name}
+            <FiX color="#737380" margin-left="1rem" />
         </button>
 
     ) : (
